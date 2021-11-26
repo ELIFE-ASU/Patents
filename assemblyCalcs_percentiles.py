63098426f8b478209250644122b883fa67b77296
jfmalloy1 <liamwilbraham@gmail.com>
2021-11-26 15:00:04 +0000

    Assembly calculations over compounds in high/low percentile ranges

--- assemblyCalcs_percentiles.py
+++ assemblyCalcs_percentiles.py
@@ -1,29 +1,8 @@
 import assemblycalculator as ac
 import multiprocessing as mp
 import pickle
-import numpy as np
-
-
-def build_month_increments(start, stop):
-    """ Build month increments in the form YYYY-MM
-
-    Args:
-        start (int): Starting year
-        stop (int): Ending year
-
-    Returns:
-        list: list of strings in the form YYYY-MM (e.g., "1980-01")
-    """
-    months = []
-    while start <= stop:
-        for month in [
-                "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
-                "11", "12"
-        ]:
-            months.append(str(start) + "-" + month)
-        start += 1
-
-    return months
+import pandas as pd
+import os
 
 
 def calculate_assembly(inchi):
@@ -41,30 +20,99 @@ def calculate_assembly(inchi):
                          "monte-carlo",
                          num_frags_hist=10000,
                          path_samples=20000)
-    return {"month": month, "inchi": inchi, "ai": ai}
+    return {"inchi": inchi, "ai": ai}
+
+
+def read_cpds(fp):
+    """ Read inchi compounds from csv files
+
+    Args:
+        fp (string): Relative file path to csv file with SureChemBL cpd data
+
+    Returns:
+        list: list of all inchis contained in the csv file
+    """
+    data = pd.read_csv(fp)
+    return data["InChI"].tolist()
+
+
+def get_changing_percentileFiles(side, precision):
+    """ Find the files which correspond to change percentiles
+
+    Args:
+        side (string): min/max, referring to the largest negative/positive changes, respectively
+        precision (string): string representation of percentile precision
+
+    Returns:
+        list: list of file names which fit the appropriate criteria
+    """
+    files = []
+    for f in os.listdir("Data/Cpd_Data/"):
+        if f.startswith("ids_change_" + side + "Percentile_" +
+                        precision) and f.endswith(".csv"):
+            files.append(f)
+
+    return files
+
+
+def get_top_percentileFiles():
+    """ Returns the csv files corresponding to compounds above the 99.99th percentile
+    of total attachment values
+
+    Returns:
+        list: list of file names which fit the appropriate criteria
+    """
+    files = []
+    for f in os.listdir("Data/Cpd_Data/"):
+        if f.startswith("ids_above99_99percentile"):
+            files.append(f)
+
+    return files
+
+
+def calculate_MAs(files):
+    """ Wrapper function for MA calculation
+
+    Args:
+        files (list): list of all files which contain relevant data
+
+    Returns:
+        Writes a file containing inchis linked with assembly values
+    """
+    for f in files:
+        cpds = read_cpds("Data/Cpd_Data/" + f)
+
+        #Set up parallelization - a bit of overhead for setting it up, but that's fine
+        pool = mp.Pool(64)
+
+        #Calculate assembly values using MC method
+        assemblies = pool.map(calculate_assembly, cpds)
+
+        pool.join()
+        pool.close()
+
+        pickle.dump(assemblies,
+                    file=open("Data/Cpd_Data/" + f[:-4] + "_assembly.p", "wb"))
 
 
 def main():
     """ Steps
     1. Read in compounds from a specific file
     2. MC assembly algorithm
-    3. Save calculations using name  + "assembly" - link inchi & MA?
+    3. Save calculations using name  + "assembly" - link inchi & MA
     4. Link assembly values to csv file (eventually, probably do this in a separate script)
     """
 
-    #Set up parallelization - a bit of overhead for setting it up, but that's fine
-    pool = mp.Pool(64)
+    ### SMALLEST & LARGEST CHANGE VALUES ###
+    #Options: min/max, 0.1/0.01
+    for option in [("min", "0.1"), ("min", "0.01"), ("max", "0.1"),
+                   ("max", "0.01")]:
+        files = get_changing_percentileFiles(option[0], option[1])
+        calculate_MAs(files)
 
-    #Calculate assembly values using MC method
-    assemblies = pool.map(calculate_assembly, data)
-
-    pool.join()
-    pool.close()
-
-    pickle.dump(assemblies,
-                file=open("results_MC.p",
-                            "wb"))
-
+    ### TOP ATTACHMENT VALUES ###
+    files = get_top_percentileFiles()
+    calculate_MAs(files)
 
 
 if __name__ == "__main__":
