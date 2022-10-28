b3943a2a23c8e920aef440d850c987d6c06c7b6d
John Malloy <jfmalloy1@gmail.com>
2022-10-28 16:55:28 +0100

    Cost & reaxys MA analysis prep

--- build_mol_files.py
+++ build_mol_files.py
@@ -24,6 +24,24 @@ def inchi_to_mol(inchi, index, database):
         pass
 
 
+def smiles_to_mol(smiles, index, database):
+    """ Translates a smiles string to a mol file and saves it in the appropriate location
+
+    Args:
+        smiles (str): smiles description of a molecule
+        index (int): identifying number of the molecule (unique to database)
+        database (str): name of database (corresponds to a directory in 'Data')
+    """
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+        print(Chem.MolToMolBlock(mol),
+              file=open(
+                  "Data/AssemblyValues/" + database + "/" + str(index) + ".mol",
+                  "w+"))
+    except:
+        pass
+
+
 def move_completed_files(fp):
     """ Moves completed MA files from analysis directory to completed directory
 
@@ -37,13 +55,17 @@ def move_completed_files(fp):
     #     os.rename(fp + "/FullDatabase/" + f, fp + "/FullDatabase_Done/" + f)
     #     os.rename(fp + "/FullDatabase/" + f[:-4] + ".mol", fp + "/FullDatabase_Done/" + f[:-4] + ".mol", )
 
-
     print("----- Moving New Database -----")
-    completed_files = [x for x in os.listdir(fp + "/NewDatabase/") if x.endswith(".txt")]
+    completed_files = [
+        x for x in os.listdir(fp + "/NewDatabase/") if x.endswith(".txt")
+    ]
 
     for f in tqdm(completed_files):
         os.rename(fp + "/NewDatabase/" + f, fp + "/NewDatabase_Done/" + f)
-        os.rename(fp + "/NewDatabase/" + f[:-4] + ".mol", fp + "/NewDatabase_Done/" + f[:-4] + ".mol", )
+        os.rename(
+            fp + "/NewDatabase/" + f[:-4] + ".mol",
+            fp + "/NewDatabase_Done/" + f[:-4] + ".mol",
+        )
 
 
 def main():
@@ -69,7 +91,56 @@ def main():
     #                                              str(x.name), "NewDatabase"),
     #                       axis=1)
 
-    move_completed_files("Data/AssemblyValues")
+    # ## Cost database - random & percentiles - to mol
+    # print("------ Cost Random  -----")
+    # cost_df = pd.read_csv("Data/Cost/random_cost_50k.csv")
+    # cost_df = cost_df.rename(columns={"Unnamed: 0": "ID"
+    #                                  })  #Make sure ID column is named correctly
+
+    # #Make sure ids are unique
+    # # print(len(set(list(cost_df["ID"])))) #Should be 50k - YEP
+    # cost_df.progress_apply(
+    #     lambda x: smiles_to_mol(x["smiles"], x["ID"], "CostRandom"), axis=1)
+
+    # print("------ Cost Percentiles  -----")
+    # cost_percentile_df = pd.read_csv(
+    #     "Data/Cost/random_cost_percentiles_50k.csv")
+    # cost_percentile_df = cost_percentile_df.rename(
+    #     columns={"Unnamed: 0": "ID"})  #Make sure ID column is named correctly)
+
+    # cost_percentile_df.progress_apply(
+    #     lambda x: smiles_to_mol(x["smiles"], x["ID"], "CostRandomPercentiles"),
+    #     axis=1)
+
+    # print("----- Reaxys Random -----")
+    # reaxys_df = pd.read_csv("Data/Reaxys/random_100k.csv")
+    # reaxys_df = reaxys_df.rename(
+    #     columns={"Unnamed: 0": "ID"})  #Make sure ID column is named correctly
+
+    # reaxys_df.progress_apply(
+    #     lambda x: smiles_to_mol(x["smiles"], x["ID"], "ReaxysRandom"), axis=1)
+
+    # print("----- Reaxys Product Percentiles -----")
+    # reaxys_df = pd.read_csv(
+    #     "Data/Reaxys/random_uniform_productPercentiles_110k.csv")
+    # reaxys_df = reaxys_df.rename(
+    #     columns={"Unnamed: 0": "ID"})  #Make sure ID column is named correctly
+
+    # reaxys_df.progress_apply(lambda x: smiles_to_mol(
+    #     x["smiles"], x["ID"], "ReaxysProductPercentiles"),
+    #                          axis=1)
+
+    print("----- Reaxys Reference Percentiles -----")
+    reaxys_df = pd.read_csv(
+        "Data/Reaxys/random_uniform_referencePercentiles_96k.csv")
+    reaxys_df = reaxys_df.rename(
+        columns={"Unnamed: 0": "ID"})  #Make sure ID column is named correctly
+
+    reaxys_df.progress_apply(lambda x: smiles_to_mol(
+        x["smiles"], x["ID"], "ReaxysReferencePercentiles"),
+                             axis=1)
+
+    # move_completed_files("Data/AssemblyValues")
 
 
 if __name__ == "__main__":
