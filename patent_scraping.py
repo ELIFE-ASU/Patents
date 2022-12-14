2bb2bc8d33c069d992afbc93af8489f594e26973
John Malloy <jfmalloy1@gmail.com>
2022-12-14 11:04:55 -0700

    Scraping patent author records

--- patent_scraping.py
+++ patent_scraping.py
@@ -68,14 +68,41 @@ def get_patentData(patent):
         print(e)
 
 
+def get_author_patents(name):
+    href_1 = "https://pubchem.ncbi.nlm.nih.gov/sdq/sdqagent.cgi?infmt=json&outfmt=json&query={%22download%22:%22*%22,%22collection%22:%22patent%22,%22where%22:{%22ands%22:["
+    ## {%22*%22:%22FRERIKS%22},{%22*%22:%22JAN%22}
+    href_2 = "]},%22order%22:[%22relevancescore,desc%22],%22start%22:1,%22limit%22:10000000}"
+    names = name.split()
+    for subname in names:
+        href_1 += "{%22*%22:%22" + subname + "%22}"
+        if subname != names[-1]:
+            href_1 += ","
+
+    href = href_1 + href_2
+    get_url = request.urlopen(href)
+
+    with open("patent_test_" + name.replace(" ", "_") + ".json", "w") as f:
+        json_data = json.loads(get_url.read())
+        json.dump(json_data, f, indent=4)
+
+
 def main():
-    fp = "Data/CpdPatentIdsDates/patent_ID_index_dict.p"
-    n = 10000
+    # #### Download n random patent IDs ####
+    # fp = "Data/CpdPatentIdsDates/patent_ID_index_dict.p"
+    # n = 10000
 
-    patents = get_patentIDs(fp, n)
+    # patents = get_patentIDs(fp, n)
 
-    for patent in tqdm(patents):
-        get_patentData(patent)
+    # for patent in tqdm(patents):
+    #     get_patentData(patent)
+
+    #### Download patents associated with authors from above n patents ####
+    names = [
+        "FRERIKS JAN", "VAN GAALEN RONALD PETRUS CLEME",
+        "KOOYMANS PETRUS GERARDUS"
+    ]
+    for name in tqdm(names):
+        get_author_patents(name)
 
 
 if __name__ == "__main__":
