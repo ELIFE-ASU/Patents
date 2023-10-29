8e607f7c4f1b2c9813fa095f8b7d9ee1acbd4381
John Malloy <jfmalloy1@gmail.com>
2023-10-29 12:08:18 -0600

    File path updates

--- get_patent_network_data.py
+++ get_patent_network_data.py
@@ -27,7 +27,7 @@ def build_month_list(start, end):
 
 def get_pickled_files(month):
     patent_date_dict =  pickle.load(open("../../../../mnt/Archive/Shared/PatentData/SureChemBL/CpdPatentIdsDates/Patent_Date_Dict/patent_date_dict_" + month + ".p", "rb"))
-    patent_cpd_edges =  pickle.load(open("../../../../mnt/Archive/Shared/PatentData/SureChemBL/CpdPatentIdsDates/Patent_Cpd_Dict/patent_cpd_edges_" + month + ".p", "rb"))
+    patent_cpd_edges =  pickle.load(open("../../../../mnt/Archive/Shared/PatentData/SureChemBL/CpdPatentIdsDates/Patent_Cpd_Edges/patent_cpd_edges_" + month + ".p", "rb"))
 
     return patent_date_dict, patent_cpd_edges
 
